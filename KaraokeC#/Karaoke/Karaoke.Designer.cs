4ab0b4cc329bd2ba2c077265b30e8ba21391a4b0
ma-sa-0122 <dosadventurenote@gmail.com>
2025-08-23 06:30:20 +0900

    update C# PitchPrinter & Karaoke

---
+++ KaraokeC#/Karaoke/Karaoke.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Karaoke
+{
+    partial class Karaoke
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartInputWave = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartPitch = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.isFourier = new System.Windows.Forms.RadioButton();
+            this.isYIN = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBoxDevices = new System.Windows.Forms.ListBox();
+            this.buttonDeviceUpdate = new System.Windows.Forms.Button();
+            this.timerUI = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.mps = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.samprate = new System.Windows.Forms.DomainUpDown();
+            this.listBoxMusic = new System.Windows.Forms.ListBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.labelLyrics = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chartInputWave)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPitch)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mps)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartInputWave
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartInputWave.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartInputWave.Legends.Add(legend1);
+            this.chartInputWave.Location = new System.Drawing.Point(562, 12);
+            this.chartInputWave.Name = "chartInputWave";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartInputWave.Series.Add(series1);
+            this.chartInputWave.Size = new System.Drawing.Size(500, 452);
+            this.chartInputWave.TabIndex = 0;
+            this.chartInputWave.TabStop = false;
+            this.chartInputWave.Text = "chart1";
+            // 
+            // chartPitch
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartPitch.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartPitch.Legends.Add(legend2);
+            this.chartPitch.Location = new System.Drawing.Point(12, 517);
+            this.chartPitch.Name = "chartPitch";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chartPitch.Series.Add(series2);
+            this.chartPitch.Size = new System.Drawing.Size(1050, 500);
+            this.chartPitch.TabIndex = 2;
+            this.chartPitch.TabStop = false;
+            this.chartPitch.Text = "chart3";
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(12, 360);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(245, 56);
+            this.buttonPlay.TabIndex = 3;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(263, 360);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(244, 56);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // isFourier
+            // 
+            this.isFourier.AutoSize = true;
+            this.isFourier.Checked = true;
+            this.isFourier.Location = new System.Drawing.Point(47, 20);
+            this.isFourier.Name = "isFourier";
+            this.isFourier.Size = new System.Drawing.Size(219, 28);
+            this.isFourier.TabIndex = 5;
+            this.isFourier.TabStop = true;
+            this.isFourier.Text = "Fourier Transform";
+            this.isFourier.UseVisualStyleBackColor = true;
+            // 
+            // isYIN
+            // 
+            this.isYIN.AutoSize = true;
+            this.isYIN.Location = new System.Drawing.Point(47, 68);
+            this.isYIN.Name = "isYIN";
+            this.isYIN.Size = new System.Drawing.Size(76, 28);
+            this.isYIN.TabIndex = 6;
+            this.isYIN.Text = "YIN";
+            this.isYIN.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.isYIN);
+            this.panel1.Controls.Add(this.isFourier);
+            this.panel1.Location = new System.Drawing.Point(12, 232);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(495, 108);
+            this.panel1.TabIndex = 7;
+            // 
+            // listBoxDevices
+            // 
+            this.listBoxDevices.FormattingEnabled = true;
+            this.listBoxDevices.ItemHeight = 24;
+            this.listBoxDevices.Location = new System.Drawing.Point(12, 62);
+            this.listBoxDevices.Name = "listBoxDevices";
+            this.listBoxDevices.Size = new System.Drawing.Size(245, 76);
+            this.listBoxDevices.TabIndex = 8;
+            // 
+            // buttonDeviceUpdate
+            // 
+            this.buttonDeviceUpdate.Location = new System.Drawing.Point(12, 12);
+            this.buttonDeviceUpdate.Name = "buttonDeviceUpdate";
+            this.buttonDeviceUpdate.Size = new System.Drawing.Size(86, 44);
+            this.buttonDeviceUpdate.TabIndex = 9;
+            this.buttonDeviceUpdate.Text = "update";
+            this.buttonDeviceUpdate.UseVisualStyleBackColor = true;
+            this.buttonDeviceUpdate.Click += new System.EventHandler(this.buttonDeviceUpdate_Click);
+            // 
+            // timerUI
+            // 
+            this.timerUI.Interval = 20;
+            this.timerUI.Tick += new System.EventHandler(this.timerUI_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 24);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "num of measure / sec : ";
+            // 
+            // mps
+            // 
+            this.mps.Location = new System.Drawing.Point(387, 195);
+            this.mps.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.mps.Name = "mps";
+            this.mps.Size = new System.Drawing.Size(120, 31);
+            this.mps.TabIndex = 12;
+            this.mps.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 24);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "sampling rate : ";
+            // 
+            // samprate
+            // 
+            this.samprate.Items.Add("32768");
+            this.samprate.Items.Add("22100");
+            this.samprate.Items.Add("16384");
+            this.samprate.Items.Add("11052");
+            this.samprate.Items.Add("8192");
+            this.samprate.Location = new System.Drawing.Point(387, 157);
+            this.samprate.Name = "samprate";
+            this.samprate.Size = new System.Drawing.Size(120, 31);
+            this.samprate.TabIndex = 14;
+            this.samprate.Text = "16384";
+            // 
+            // listBoxMusic
+            // 
+            this.listBoxMusic.FormattingEnabled = true;
+            this.listBoxMusic.ItemHeight = 24;
+            this.listBoxMusic.Location = new System.Drawing.Point(263, 62);
+            this.listBoxMusic.Name = "listBoxMusic";
+            this.listBoxMusic.Size = new System.Drawing.Size(244, 76);
+            this.listBoxMusic.TabIndex = 15;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 422);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(495, 90);
+            this.trackBar1.TabIndex = 16;
+            // 
+            // labelLyrics
+            // 
+            this.labelLyrics.AutoSize = true;
+            this.labelLyrics.Font = new System.Drawing.Font("MS UI Gothic", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelLyrics.Location = new System.Drawing.Point(12, 483);
+            this.labelLyrics.Name = "labelLyrics";
+            this.labelLyrics.Size = new System.Drawing.Size(71, 29);
+            this.labelLyrics.TabIndex = 17;
+            this.labelLyrics.Text = "歌詞";
+            // 
+            // Karaoke
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1074, 1029);
+            this.Controls.Add(this.labelLyrics);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.listBoxMusic);
+            this.Controls.Add(this.samprate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.mps);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDeviceUpdate);
+            this.Controls.Add(this.listBoxDevices);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.chartPitch);
+            this.Controls.Add(this.chartInputWave);
+            this.MaximizeBox = false;
+            this.Name = "Karaoke";
+            this.Text = "Karaoke";
+            this.Shown += new System.EventHandler(this.PitchPrinter_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.chartInputWave)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPitch)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mps)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartInputWave;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartPitch;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.RadioButton isFourier;
+        private System.Windows.Forms.RadioButton isYIN;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBoxDevices;
+        private System.Windows.Forms.Button buttonDeviceUpdate;
+        private System.Windows.Forms.Timer timerUI;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown mps;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DomainUpDown samprate;
+        private System.Windows.Forms.ListBox listBoxMusic;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label labelLyrics;
+    }
+}
+
---
+++ KaraokeC#/Karaoke/Karaoke.cs
@@ -0,0 +1,410 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using NAudio.Dsp;
+using NAudio.Wave;
+using MathNet.Numerics;
+using MathNet.Numerics.IntegralTransforms;
+using System.Numerics;
+using Series = System.Windows.Forms.DataVisualization.Charting.Series;
+using Complex = System.Numerics.Complex;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace Karaoke
+{
+    public partial class Karaoke : Form
+    {
+        private int samplingRate = 16384;
+        private int samplingInterval = 512;
+        private int arrayRange;
+        private int window;
+        const int VALID_MAX_FREQ = 1000;
+
+        private WaveInEvent waveIn;
+        private WaveOutEvent waveOut;
+        private AudioFileReader music;
+
+        private double[] hanningWindow;
+        private double[] pitchArray;
+        private int pitchIndex = 0;
+
+        private StripLine playLine; // 再生位置のガイド線
+
+
+        public Karaoke()
+        {
+            this.AutoScaleMode = AutoScaleMode.Dpi;
+            InitializeComponent();
+            SetDeviceList();
+            SetupCharts();
+
+            Task.Run(AnalysisLoop);
+            timerUI.Start();
+        }
+
+        private void PitchPrinter_Shown(object sender, EventArgs e)
+        {
+            // グラフにダミーデータを与えて描画。
+            // 最初の描画にかかる初期化コストをここで受ける。
+            chartInputWave.Series[0].Points.AddXY(1, 0);
+            chartInputWave.Update();
+
+            chartPitch.Series[0].Points.AddXY(1, 0);
+            chartPitch.Update();
+        }
+
+        private void UpdateParameters()
+        {
+            window = samplingInterval / 2;
+            hanningWindow = new double[samplingInterval];
+            MakeHanningTable(); // ハニング窓再生成
+
+            UpdatePhaseSec(5.0);
+        }
+
+        private void UpdatePhaseSec(double second)
+        {
+            arrayRange = (int)(samplingRate / samplingInterval * second);
+            pitchArray = new double[arrayRange];
+        }
+
+
+        private void SetupCharts()
+        {
+            // 波形チャート設定
+            SetupChart(chartInputWave, -1, 1, samplingInterval, samplingInterval, "Waveform");
+
+            // ピッチチャート設定
+            SetupPitchChart(chartPitch);
+        }
+
+        private void SetupChart(Chart chart, double ymin, double ymax, double xmax, int interval, string seriesName)
+        {
+            chart.Series.Clear();
+            var series = new Series(seriesName)
+            {
+                ChartType = SeriesChartType.FastLine
+            };
+            chart.Series.Add(series);
+
+            // ChartArea設定
+            var area = chart.ChartAreas[0];
+            area.AxisY.Minimum = ymin;
+            area.AxisY.Maximum = ymax;
+            area.AxisX.Minimum = 0;
+            area.AxisX.Maximum = xmax;
+            area.AxisX.Interval = interval;
+
+            // 凡例設定
+            chart.Legends.Add(new Legend("Default"));
+            chart.Legends[0].Docking = Docking.Top;
+            chart.Legends[0].Alignment = StringAlignment.Far;
+            chart.Legends[0].IsDockedInsideChartArea = true;
+
+            // 余白調整（例: Chart全体内の描画位置調整）
+            area.Position.Auto = false;
+            area.Position.X = 10;
+            area.Position.Y = 10;
+            area.Position.Width = 100;
+            area.Position.Height = 90;
+        }
+
+        private void SetupPitchChart(Chart chartPitch)
+        {
+            SetupChart(chartPitch, 0, 12, arrayRange, arrayRange, "Pitch");
+            ChangePitchLabels(chartPitch);
+
+            playLine = new StripLine();
+            playLine.Interval = 0;
+            playLine.StripWidth = 0;              // 幅0で「線」
+            playLine.BorderColor = Color.Red;
+            playLine.BorderWidth = 2;
+            chartPitch.ChartAreas[0].AxisX.StripLines.Add(playLine);
+        }
+
+
+        private void ChangePitchLabels(Chart chartPitch)
+        {
+            var area = chartPitch.ChartAreas[0];
+            area.AxisY.Interval = 1;
+
+            // 音階ラベル設定
+            string[] noteNames = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+            area.AxisY.CustomLabels.Clear();
+            for (int i = 0; i < 12; i++)
+            {
+                int noteIndex = i % 12;
+                CustomLabel label = new CustomLabel(i - 0.5, i + 0.5, noteNames[noteIndex], 0, LabelMarkStyle.None);
+                area.AxisY.CustomLabels.Add(label);
+            }
+        }
+
+        private void MakeHanningTable()
+        {
+            for (int i = 0; i < samplingInterval; i++)
+            {
+                hanningWindow[i] = 0.5 - 0.5 * Math.Cos(2.0 * Math.PI * i / (samplingInterval - 1));
+            }
+        }
+
+
+        private void SetDeviceList()
+        {
+            listBoxDevices.Items.Clear();
+            for (int i = 0; i < WaveIn.DeviceCount; i++)
+            {
+                var capabilities = WaveIn.GetCapabilities(i);
+                listBoxDevices.Items.Add(capabilities.ProductName);
+            }
+        }
+
+        private void SetMusicList()
+        {
+
+        }
+
+
+        /*
+         イベント
+         */
+
+        private void buttonDeviceUpdate_Click(object sender, EventArgs e)
+        {
+            SetDeviceList();
+            SetMusicList();
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            buttonPlay.Enabled = false;
+            buttonStop.Enabled = true;
+
+            /* 変数の設定 */
+            samplingRate = int.Parse(samprate.Text);
+            samplingInterval = samplingRate / (int)mps.Value;
+            UpdateParameters();
+
+            /* ピッチチャートの幅を調整 */
+            var area = chartPitch.ChartAreas[0];
+            area.AxisX.Minimum = 0;
+            area.AxisX.Maximum = arrayRange;
+            area.AxisX.Interval = arrayRange;
+
+            /* WaveIn でマイク入力 */
+            waveIn = new WaveInEvent
+            {
+                DeviceNumber = listBoxDevices.SelectedIndex,
+                WaveFormat = new WaveFormat(samplingRate, 16, 1),
+                BufferMilliseconds = (int)((double)samplingInterval / samplingRate * 1000)
+            };
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+            waveIn.StartRecording();
+
+            /* WaveOut で音楽再生 */
+            //waveOut = new WaveOutEvent();
+            //music = new AudioFileReader(listBoxMusic.Text);
+            //waveOut.Init(music);
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            buttonPlay.Enabled = true;
+            buttonStop.Enabled = false;
+
+            if (waveIn != null)
+            {
+                waveIn.StopRecording();
+                waveIn.Dispose();
+                waveIn = null;
+            }
+            //if (waveOut != null)
+            //{
+            //    waveOut.Pause();
+            //    waveOut.Dispose();
+            //    waveOut = null;
+            //}
+        }
+
+
+        /*
+         音声受け取り・描画
+         */
+
+        // マイク入力用バッファ（リングバッファ）
+        private readonly ConcurrentQueue<double[]> audioQueue = new ConcurrentQueue<double[]>();
+        private readonly ConcurrentQueue<double[]> analysisQueue = new ConcurrentQueue<double[]>();
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            short[] buffer = new short[e.BytesRecorded / 2];
+            Buffer.BlockCopy(e.Buffer, 0, buffer, 0, e.BytesRecorded);
+
+            double[] processed = new double[e.BytesRecorded / 2];
+            for (int i = 0; i < buffer.Length; i++)
+                processed[i] = buffer[i] / (double)short.MaxValue;
+
+            audioQueue.Enqueue(processed); // 音声データだけ積む（UI更新しない）
+        }
+
+        // 別スレッドで解析
+        private async Task AnalysisLoop()
+        {
+            while (true)
+            {
+                if (audioQueue.TryDequeue(out var data))
+                {
+                    var pitch = DetectPitch(data);
+                    if (pitchIndex >= pitchArray.Length) {
+                        pitchIndex = 0;
+                        Array.Clear(pitchArray, 0, pitchArray.Length);
+                    }
+                    pitchArray[pitchIndex] = pitch;
+                    pitchIndex++;
+                    analysisQueue.Enqueue(data);
+                }
+                else {
+                    await Task.Delay(1); // CPU過負荷防止
+                }
+            }
+        }
+
+        // UIスレッド側はタイマーで一定時間ごとに描画
+        private void timerUI_Tick(object sender, EventArgs e)
+        {
+            while (analysisQueue.TryDequeue(out var waveform))
+            {
+                // 波形描画
+                var seriesWave = chartInputWave.Series[0];
+                seriesWave.Points.Clear();
+                for (int i = 0; i < waveform.Length; i++)
+                    seriesWave.Points.AddXY(i, waveform[i]);
+
+                // ピッチ描画
+                var seriesPitch = chartPitch.Series[0];
+                seriesPitch.Points.Clear();
+                for (int i = 0; i < arrayRange; i++)
+                {
+                    double val = pitchArray[i];
+                    seriesPitch.Points.AddXY(i, val >= 0 ? val : double.NaN);
+                }
+                playLine.IntervalOffset = (pitchIndex);
+            }
+        }
+
+
+        /*
+         ピッチ解析
+         */
+
+        private double DetectPitch(double[] x)
+        {
+            double energy = 0.0;
+            for (int i = 0; i < x.Length; i++)
+                energy += x[i] * x[i];
+
+            if (energy < 1e-4)
+                return -1;
+
+            double frequency = 0;
+            if (isFourier.Checked)
+                frequency = FourierTransform(x);
+            else
+                frequency = YIN(x);
+
+            // 無音判定。85Hz ~ 1000Hz を有効範囲とする。
+            if (frequency < 85 || frequency > VALID_MAX_FREQ)
+                return -1;
+
+            // ピッチへの変換
+            double midiNote = 69 + 12 * Math.Log(frequency / 440.0, 2);
+            double pitchClass = midiNote % 12;
+
+            return pitchClass;
+        }
+
+        private double YIN(double[] x)
+        {
+            double[] YIN = new double[window];
+            double[] cumAve = new double[window];
+            double sum = 0;
+            int lamda = 0;
+
+            for (int tau = 0; tau < window; tau++)
+            {
+                for (int j = 0; j < window - tau; j++)
+                {
+                    double diff = x[j] - x[j + tau];
+                    YIN[tau] += diff * diff;
+                }
+            }
+
+            cumAve[0] = 1;
+            bool flag = false;
+            for (int tau = 1; tau < window; tau++)
+            {
+                sum += YIN[tau];
+                cumAve[tau] = YIN[tau] / (sum / tau);
+
+                if (!flag) 
+                {
+                    if (cumAve[tau - 1] < 0.15 && cumAve[tau - 1] < cumAve[tau])
+                    {
+                        lamda = tau - 1;
+                        flag = true;
+                    }
+                }
+            }
+
+            if (lamda == 0)
+                return -1;
+
+            double betterFreq = lamda + (cumAve[lamda - 1] - cumAve[lamda + 1]) /
+                                (2.0 * (cumAve[lamda - 1] - 2.0 * cumAve[lamda] + cumAve[lamda + 1]));
+
+            double detectedFreq = samplingRate / betterFreq;
+
+            return detectedFreq;
+        }
+
+        private double FourierTransform(double[] x)
+        {
+            // 必要なサイズだけコピー（不足分は0）
+            Complex[] buffer = new Complex[samplingRate];
+            int length = Math.Min(x.Length, buffer.Length);
+            for (int i = 0; i < length; i++)
+            {
+                buffer[i] = new Complex(x[i], 0.0);
+            }
+
+            // FFT 実行（破壊的に変換）
+            Fourier.Forward(buffer, FourierOptions.Matlab); // Matlab形式は実数信号に適している
+
+            // 最もスペクトルの強い index を調べる
+            double maxPower = 0.0;
+            int maxIndex = 0;
+            for (int i = 1; i < (buffer.Length / 2); i++)
+            {
+                double power = buffer[i].Magnitude * buffer[i].Magnitude;
+                if (power > maxPower)
+                {
+                    maxPower = power;
+                    maxIndex = i;
+                }
+            }
+
+            // 周波数 = index * Fs / N
+            // 分解能[Hz]は 周波数 / データ点数。1000Hzの入力を500点で計算したら2Hz区切りでしか取れない感じ
+            double dominantFreq = (double)maxIndex * samplingRate / (double)buffer.Length;
+
+            return dominantFreq;
+        }
+    }
+}
